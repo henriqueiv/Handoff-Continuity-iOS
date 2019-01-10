cfd917e0b50c091fe6feb1462ec86e577f6acc9c
Henrique Valcanaia <henriqueindalencio@gmail.com>
2019-01-10 21:21:10 -0200

    Commit before backup

--- HandoffContinuity/ViewController.swift
+++ HandoffContinuity/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createUserActivity()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func createUserActivity(){
+        let activity = NSUserActivity(activityType: "xyz.henriquevalcanaia.HandoffContinuity.notes.edit")
+        activity.title = "Editing"
+        activity.userInfo = ["title": titleTextField.text!, "content": contentTextField.text]
+        self.userActivity = activity;
+        self.userActivity?.becomeCurrent()
     }
-
-
+    
+    override func updateUserActivityState(activity: NSUserActivity) {
+        activity.addUserInfoEntriesFromDictionary(["title": titleTextField.text!, "content": contentTextField.text])
+        super.updateUserActivityState(activity)
+    }
+    
+    deinit {
+        userActivity?.invalidate()
+    }
+    
+    override func restoreUserActivityState(activity: NSUserActivity) {
+        if let userInfo:[NSObject:AnyObject] = activity.userInfo{
+            titleTextField.text = userInfo["title"] as? String
+            contentTextField.text = userInfo["content"] as? String
+        }
+    }
+    
 }
 
